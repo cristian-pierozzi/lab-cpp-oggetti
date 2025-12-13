7c05a3525a47ed539aa00d99cc1f39a574a8db43
cristian pierozzi <cristianpierozzi7@gmail.com>
2025-12-13 11:39:45 +0100

    Update 3_userGenerator.cpp

--- 1-string/3_userGenerator.cpp
+++ 1-string/3_userGenerator.cpp
@@ -8,6 +8,7 @@
  #include <iostream>
 #include <string>
 #include <cctype>
+#include<sstream>
 
 using namespace std;
 
@@ -25,22 +26,39 @@ string minuscolo(string s){
           prima lettera del nome + tutto il cognome (tutto minuscolo)
 */
 string usernameGenerator(string nome, string cognome){
-    return "da implementare";
+
+
+    string user= nome.substr(0,1);
+
+    user += cognome;
+    return minuscolo(user);
 }
 
 /*
     TODO: Implementare la funzione affinchè, a partire da 1 stringa nella forma "Nome Cognome"
           generi in output una stringa contenente uno username utente nella forma:
           prima lettera del nome + tutto il cognome (tutto minuscolo)
-    
+
     Hint: rispetto alla funzione precedente, splittare (spezzare il nome dal cognome, basandosi sullo spazio)
           e ripetere la stessa logica. E' possibile anche evitare di ripetere la logica? Magari chiamando la stessa funzione sopra?
 
 */
 string usernameGenerator(string nomeCognome){
-    return "da implementare";
-}
 
+
+stringstream ss(nomeCognome);
+    string nome;
+    string cognome;
+
+    // 2. Estrae la prima parola (il nome) e la salva in 'nome'
+    ss >> nome;
+
+    // 3. Estrae la seconda parola (il cognome) e la salva in 'cognome'
+    ss >> cognome;
+
+    // 4. Riutilizzo la prima funzione già implementata: DRY (Don't Repeat Yourself)
+    return usernameGenerator(nome, cognome);
+}
 // main con testing
 int main()
 {
@@ -54,7 +72,8 @@ int main()
 
     string username2 = usernameGenerator(nomeCognome);
 
-    cout << "Primo utente: " << nome << " " << cognome << ". Username: " << username1;
+    cout << "Primo utente: " << nome << " " << cognome << ". Username: " << username1<<endl;
+    cout<<"-----------------"<<endl;
     cout << "Secondo utente: " << nomeCognome << ". Username: " << username2;
 
     return 0;
